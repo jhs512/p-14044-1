cba58aa396376f38c4640256c53cf41588b18540
jhs512 <jangka512@gmail.com>
2025-09-02 17:27:59 +0900

    work

--- backend/src/main/kotlin/com/back/global/app/AppConfig.kt
+++ backend/src/main/kotlin/com/back/global/app/AppConfig.kt
@@ -2,6 +2,7 @@ package com.back.global.app
 
 import com.back.standard.util.Ut
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
@@ -11,10 +12,16 @@ import org.springframework.security.crypto.password.PasswordEncoder
 @Configuration
 class AppConfig(
     environment: Environment,
-    objectMapper: ObjectMapper
+    objectMapper: ObjectMapper,
+    @Value("\${custom.site.cookieDomain}") cookieDomain: String,
+    @Value("\${custom.site.frontUrl}") siteFrontUrl: String,
+    @Value("\${custom.site.backUrl}") siteBackUrl: String,
 ) {
     init {
         Companion.environment = environment
+        _cookieDomain = cookieDomain
+        _siteFrontUrl = siteFrontUrl
+        _siteBackUrl = siteBackUrl
         Ut.json.objectMapper = objectMapper
     }
 
@@ -37,5 +44,13 @@ class AppConfig(
 
         val isNotProd: Boolean
             get() = !isProd
+
+        private lateinit var _cookieDomain: String
+        private lateinit var _siteFrontUrl: String
+        private lateinit var _siteBackUrl: String
+
+        val cookieDomain: String by lazy { _cookieDomain }
+        val siteFrontUrl: String by lazy { _siteFrontUrl }
+        val siteBackUrl: String by lazy { _siteBackUrl }
     }
 }
