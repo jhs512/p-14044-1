968c57db4749f3118272916ef9794c9afe93a6ae
jhs512 <jangka512@gmail.com>
2025-09-12 09:49:06 +0900

    work

--- backend/src/main/kotlin/com/back/global/app/AppConfig.kt
+++ backend/src/main/kotlin/com/back/global/app/AppConfig.kt
@@ -8,6 +8,7 @@ import com.back.domain.post.postUser.entity.PostUser
 import com.back.domain.post.postUser.repository.PostUserAttrRepository
 import com.back.standard.util.Ut
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
@@ -21,6 +22,9 @@ class AppConfig(
     memberAttrRepository: MemberAttrRepository,
     postUserAttrRepository: PostUserAttrRepository,
     memberRepository: MemberRepository,
+    @Value("\${custom.site.cookieDomain}") cookieDomain: String,
+    @Value("\${custom.site.frontUrl}") siteFrontUrl: String,
+    @Value("\${custom.site.backUrl}") siteBackUrl: String,
 ) {
     init {
         Companion.environment = environment
@@ -29,6 +33,10 @@ class AppConfig(
         BaseMember.memberAttrRepository = memberAttrRepository
         Member.attrRepository = memberAttrRepository
         PostUser.attrRepository = postUserAttrRepository
+
+        _cookieDomain = cookieDomain
+        _siteFrontUrl = siteFrontUrl
+        _siteBackUrl = siteBackUrl
     }
 
     @Bean
@@ -50,5 +58,13 @@ class AppConfig(
 
         val isNotProd: Boolean
             get() = !isProd
+
+        private lateinit var _cookieDomain: String
+        private lateinit var _siteFrontUrl: String
+        private lateinit var _siteBackUrl: String
+
+        val cookieDomain: String by lazy { _cookieDomain }
+        val siteFrontUrl: String by lazy { _siteFrontUrl }
+        val siteBackUrl: String by lazy { _siteBackUrl }
     }
 }
